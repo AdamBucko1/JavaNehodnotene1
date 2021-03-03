bc40dbfbf30b87bfdb27e0ba3eb9162027cf2ca1
AdamBucko1 <xbuckoa@stuba.sk>
2021-03-03 19:28:01 +0100

    Miny-Hra2

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,9 +1,25 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.utility.Hra;
+import sk.stuba.fei.uim.oop.utility.ZKlavesnice;
+
+
 public class Main {
     public static void main(String[] args) {
-
-
+        Hra objektHra= new Hra();
+    objektHra.VytvoreniePlochy(ZKlavesnice.readInt("Zadaj velkost hernej plochy"));
+    while (true) {
+        System.out.println("Pre ukoncenie hry napis 666");
+        int YPozicia = ZKlavesnice.readInt("Y:");
+        if(YPozicia==666){
+            break;
+        }
+        int XPozicia = ZKlavesnice.readInt("X:");
+        YPozicia = YPozicia - 1;
+        XPozicia = XPozicia - 1;
+        objektHra.HladaniePoli(YPozicia, XPozicia);
+        objektHra.VykreslenieHracejPlochy();
+    }
 
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/utility/Hra.java
+++ src/main/java/sk/stuba/fei/uim/oop/utility/Hra.java
@@ -3,34 +3,147 @@ import java.util.Random;
 
 public class Hra {
     private Random NahodnyVyber=new Random();
-    private int BlockComparisonValue=NahodnyVyber.nextInt(100);
+    public int[][] PoleHracejPlochy;
+    public int SizeOfBoard;
     public int Obtiaznost;
-
+    public int BlockComparisonValue=0;
     public void VytvoreniePlochy(int velkost){
+        Obtiaznost=ZKlavesnice.readInt("Zadaj obtiaznost od 1 do 10");
+        PoleHracejPlochy=new int[velkost][velkost];
+        SizeOfBoard=velkost;
 
-        for(int riadok=0;riadok<velkost;riadok++){
-            for(int stlpec=0;stlpec<velkost;stlpec++){
-                VyberPolicka();
+
+        for(int stlpec=0;stlpec<velkost;stlpec++){
+            for(int riadok=0;riadok<velkost;riadok++){
+                int BlockComparisonValueVCykle=NahodnyVyber.nextInt(100);
+                BlockComparisonValue=BlockComparisonValueVCykle;
+                PoleHracejPlochy[stlpec][riadok]=VyberPolicka();
+
+
+            }
+
+        }
+    VykreslenieHracejPlochy();
+    }
+    public void HladaniePoli(int CurrentY, int CurrentX){
+        if(PoleHracejPlochy[CurrentY][CurrentX]==0) {
+            int HodnotaPola=2;
+
+            if(CurrentX<SizeOfBoard-1){
+            if(PoleHracejPlochy[CurrentY][CurrentX+1]==1){
+                HodnotaPola=HodnotaPola+1;
+            }}
+            if(CurrentX>0){
+            if(PoleHracejPlochy[CurrentY][CurrentX-1]==1){
+                HodnotaPola=HodnotaPola+1;
+            }}
+            if(CurrentY<SizeOfBoard-1 && CurrentX<SizeOfBoard-1){
+            if(PoleHracejPlochy[CurrentY+1][CurrentX+1]==1){
+                HodnotaPola=HodnotaPola+1;
+            }}
+            if(CurrentY<SizeOfBoard-1 && CurrentX>0){
+            if(PoleHracejPlochy[CurrentY+1][CurrentX-1]==1){
+                HodnotaPola=HodnotaPola+1;
+            }}
+            if(CurrentX>0 && CurrentY>0){
+            if(PoleHracejPlochy[CurrentY-1][CurrentX-1]==1){
+                HodnotaPola=HodnotaPola+1;
+            }}
+            if(CurrentX<SizeOfBoard-1 && CurrentY>0){
+            if(PoleHracejPlochy[CurrentY-1][CurrentX+1]==1){
+                HodnotaPola=HodnotaPola+1;
+            }}
+            if(CurrentY>0){
+            if(PoleHracejPlochy[CurrentY-1][CurrentX]==1){
+                HodnotaPola=HodnotaPola+1;
+            }}
+            if(CurrentY<SizeOfBoard-1){
+            if(PoleHracejPlochy[CurrentY+1][CurrentX]==1){
+                HodnotaPola=HodnotaPola+1;
+            }}
+            PoleHracejPlochy[CurrentY][CurrentX]=HodnotaPola;
+
+            if(CurrentY<SizeOfBoard-1){
+                HladaniePoli(CurrentY+1, CurrentX);
+            }
+            if(CurrentY>0){
+                HladaniePoli(CurrentY-1, CurrentX);
+            }
+            if(CurrentX<SizeOfBoard-1){
+                HladaniePoli(CurrentY, CurrentX+1);
+            }
+            if(CurrentX>0){
+                HladaniePoli(CurrentY, CurrentX-1);
+            }
+
+
+        }
+
+    }
+
+    public void VykreslenieHracejPlochy(){
+        int VykreslenieY=0;
+        int VykreslenieX=0;
+        int Cyklus1=0;
+        for (Cyklus1=0;Cyklus1<SizeOfBoard;Cyklus1++){
+            if (Cyklus1==0){
+                System.out.print(" ");
+            }
+            if (Cyklus1<9){
+                System.out.print(" ");
+            }
+            System.out.print(" "+(Cyklus1+1));
+            if (Cyklus1==SizeOfBoard-1){
+                System.out.println("");
+            }
+        }
+        for (VykreslenieY=0;VykreslenieY<SizeOfBoard;VykreslenieY++){
+            if (VykreslenieY<9){
+                System.out.print(" ");
+            }
+            System.out.print(VykreslenieY+1);
+            for (VykreslenieX=0;VykreslenieX<SizeOfBoard;VykreslenieX++){
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==0){
+                    System.out.print(" o ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==1){
+                    System.out.print(" x ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==2){
+                    System.out.print("   ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==3){
+                    System.out.print(" 1 ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==4){
+                    System.out.print(" 2 ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==5){
+                    System.out.print(" 3 ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==6){
+                    System.out.print(" 4 ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==7){
+                    System.out.print(" 5 ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==8){
+                    System.out.print(" 6 ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==9){
+                    System.out.print(" 7 ");}
+                if (PoleHracejPlochy[VykreslenieY][VykreslenieX]==10){
+                    System.out.print(" 8 ");}
             }
             System.out.println("");
         }
 
     }
+        private int VyberPolicka(){
+        int MineBlock;
+        MineBlock=BlockGenerator();
 
-        private void VyberPolicka(){
-        int SafeBlock;
-        SafeBlock=BlockGenerator(Obtiaznost);
-
-        if(SafeBlock==1){
-            System.out.print("o");
+        if(MineBlock==1){
+            return 0;
         }
         else{
-            System.out.print("x");
+            return 1;
         }
 
         }
 
-        private int BlockGenerator(int Obtiaznost){
+        private int BlockGenerator(){
         if (Obtiaznost>BlockComparisonValue){
             return 0;
         }
@@ -41,4 +154,28 @@ public class Hra {
 
 
         }
+
+    public Random getNahodnyVyber() {
+        return NahodnyVyber;
+    }
+
+    public void setNahodnyVyber(Random nahodnyVyber) {
+        NahodnyVyber = nahodnyVyber;
+    }
+
+    public int getBlockComparisonValue() {
+        return BlockComparisonValue;
+    }
+
+    public void setBlockComparisonValue(int blockComparisonValue) {
+        BlockComparisonValue = blockComparisonValue;
+    }
+
+    public int getObtiaznost() {
+        return Obtiaznost;
+    }
+
+    public void setObtiaznost(int obtiaznost) {
+        Obtiaznost = obtiaznost;
+    }
 }
